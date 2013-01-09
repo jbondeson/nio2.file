64b3540ff181d45b2f8169cd6f0eea7783f494a1
Jeremy Bondeson <jbondeson@gmail.com>
2013-01-08 21:34:29 -0700

    Updated project file with license and url.

--- project.clj
+++ project.clj
@@ -1,3 +1,7 @@
-(defproject nio2.file "0.1.0"
+(defproject nio2.file "0.1.0-SNAPSHOT"
   :description "Clojure wrapper for the Java NIO.2 file namespace"
+  :url "http://github.com/jbondeson/nio2.file"
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo}
   :dependencies [[org.clojure/clojure "1.4.0"]])
